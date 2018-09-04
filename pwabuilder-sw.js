6b3c17a67d0c013d08c01906cbc8c4d90733731d
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2018-09-04 23:50:50 +0300

    Alpha 1.0v

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -20,27 +20,27 @@
 // });
 
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(
-                [
-                    '/',
-                    '/css/',
-                    '/dist/',
-                    '/images/',
-                    '/js/',
-                    '/moment/',
-                    '/node_modules/',
-                    '/src',
-                    'index.html',
-                    'manifest.json',
-                    '/offline.html'
-                ]
-            );
-        })
-    );
-});
+// self.addEventListener('install', function(event) {
+//     event.waitUntil(
+//         caches.open(cacheName).then(function(cache) {
+//             return cache.addAll(
+//                 [
+//                     '/',
+//                     '/css/',
+//                     '/dist/',
+//                     '/images/',
+//                     '/js/',
+//                     '/moment/',
+//                     '/node_modules/',
+//                     '/src',
+//                     'index.html',
+//                     'manifest.json',
+//                     '/offline.html'
+//                 ]
+//             );
+//         })
+//     );
+// });
 
 
 
@@ -61,25 +61,105 @@ self.addEventListener('install', function(event) {
 //     }));
 // });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.open('mysite-dynamic').then(function(cache) {
-            return cache.match(event.request).then(function (response) {
-                return response || fetch(event.request).then(function(response) {
-                    cache.put(event.request, response.clone());
-                    return response;
-                });
-            });
-        })
-    );
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//         caches.open('mysite-dynamic').then(function(cache) {
+//             return cache.match(event.request).then(function (response) {
+//                 return response || fetch(event.request).then(function(response) {
+//                     cache.put(event.request, response.clone());
+//                     return response;
+//                 });
+//             });
+//         })
+//     );
+// });
+//
+//
+//
+// //This is a event that can be fired from your page to tell the SW to update the offline page
+// self.addEventListener('refreshOffline', function(response) {
+//   return caches.open('pwabuilder-offline').then(function(cache) {
+//     console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
+//     return cache.put(offlinePage, response);
+//   });
+// });
+
+
+
+//This is the service worker with the Cache-first network
+
+var CACHE = 'pwabuilder-precache';
+var precacheFiles = [
+    '/',
+                    '/css/',
+                    '/dist/',
+                    '/images/',
+                    '/js/',
+                    '/moment/',
+                    '/node_modules/',
+                    '/src',
+                    'index.html',
+                    'manifest.json',
+                    '/offline.html'
+    /* Add an array of files to precache for your app */
+];
+
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function(evt) {
+    console.log('[PWA Builder] The service worker is being installed.');
+    evt.waitUntil(precache().then(function() {
+        console.log('[PWA Builder] Skip waiting on install');
+        return self.skipWaiting();
+    }));
 });
 
 
-
-//This is a event that can be fired from your page to tell the SW to update the offline page
-self.addEventListener('refreshOffline', function(response) {
-  return caches.open('pwabuilder-offline').then(function(cache) {
-    console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
-    return cache.put(offlinePage, response);
-  });
+//allow sw to control of current page
+self.addEventListener('activate', function(event) {
+    console.log('[PWA Builder] Claiming clients for current page');
+    return self.clients.claim();
 });
+
+self.addEventListener('fetch', function(evt) {
+    console.log('[PWA Builder] The service worker is serving the asset.'+ evt.request.url);
+    evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+    evt.waitUntil(update(evt.request));
+});
+
+
+function precache() {
+    return caches.open(CACHE).then(function (cache) {
+        return cache.addAll(precacheFiles);
+    });
+}
+
+function fromCache(request) {
+    //we pull files from the cache first thing so we can show them fast
+    return caches.open(CACHE).then(function (cache) {
+        return cache.match(request).then(function (matching) {
+            return matching || Promise.reject('no-match');
+        });
+    });
+}
+
+function update(request) {
+    //this is where we call the server to get the newest version of the
+    //file to use the next time we show view
+    return caches.open(CACHE).then(function (cache) {
+        return fetch(request).then(function (response) {
+            return cache.put(request, response);
+        });
+    });
+}
+
+function fromServer(request){
+    //this is the fallback if it is not in the cache to go to the server and get it
+    return fetch(request).then(function(response){ return response});
+}
+
+
+
+
+
+
+
