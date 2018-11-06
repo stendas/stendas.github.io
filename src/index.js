efda7c1f464f4642ebc85133f3663ef8b5d390ca
Marius Gegoevskis <m.gzegozevskis@eif.viko.lt>
2018-11-06 20:30:22 +0200

    tidy up some code v2.0 alpha

---
+++ src/index.js
@@ -0,0 +1,66 @@
+let Keyboard = window.SimpleKeyboard.default;
+
+let keyboard = new Keyboard({
+    onChange: input => onChange(input),
+    onKeyPress: button => onKeyPress(button),
+    layout: {
+        default: [
+            "\u0105 \u010D \u0119 \u0117 \u012F \u0161 \u0173 \u016B \u017E {bksp}  ",
+            "q w e r t y u i o p 7 8 9",
+            "\ a s d f g h j k l 4 5 6",
+            "\ z x c v b n m \uD83D\uDE0E 0 1 2 3"
+        ]
+    },
+    display: {
+        "{enter}": "submit",
+        "{bksp}": "backspace",
+        "{lock}": "lock",
+        "{shift}": "shift",
+        "{tab}": "tab",
+        "{space}": " "
+    },
+    layoutName: "default",
+    theme: "hg-theme-default emoji-theme"
+});
+
+/**
+ * Update simple-keyboard when input is changed directly
+ */
+document.querySelector(".quicksearch").addEventListener("change", event => {
+    keyboard.setInput(event.target.value);
+
+});
+
+console.log(keyboard);
+
+function onChange(input) {
+
+   // $(".quicksearch").focus();
+
+    document.querySelector(".quicksearch").value = input;
+
+    var e = $.Event( "keyup", { keyCode: 13 } );
+    $('.quicksearch').trigger(e);
+
+    console.log("The Unicode value is: " + input);
+    console.log("Input changed", input);
+}
+
+function onKeyPress(button) {
+    console.log("Button pressed", button);
+
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") handleShift();
+}
+
+function handleShift() {
+    let currentLayout = keyboard.options.layoutName;
+    let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+    keyboard.setOptions({
+        layoutName: shiftToggle
+    });
+}
